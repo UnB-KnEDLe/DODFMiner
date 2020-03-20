af7d412c306325364e5ba70ad06ade28a45c145a
maffei2443 <leoitu22hotmail.com@gmail.com>
2020-03-20 11:10:27 -0300

    refactoring code according to PEP8; also removing portuguese comments

--- extractor/title_filter.py
+++ extractor/title_filter.py
@@ -3,7 +3,7 @@
 import functools
 
 
-class NegritoCaixaAlta:
+class BoldUpperCase:
     """
     Static class to return filter functions useful
     for bold and upper case text.
@@ -25,21 +25,21 @@ class NegritoCaixaAlta:
         """Evaluates to true if d['text'] matches the following conditions:
             - all letters are uppercase
             - does not contain 4 or more consecutive spaces
-            - has a len greater than NegritoCaixaAlta.TEXT_MIN
+            - has a len greater than BoldUpperCase.TEXT_MIN
         ."""
         t = d['text'].strip().strip('.')
         cond1 = 4 * " " not in t
-        cond2 = len(t) > NegritoCaixaAlta.TEXT_MIN
+        cond2 = len(t) > BoldUpperCase.TEXT_MIN
         cond3 = t == t.upper()
         return cond1 and cond2 and cond3
 
     @staticmethod
     def dict_bold(d):
         """Evaluates do True if d['flags'] matches the following conditions:
-            - is one of the values in NegritoCaixaAlta.BOLD_FLAGS
+            - is one of the values in BoldUpperCase.BOLD_FLAGS
         ."""
         flags = d['flags']
-        return flags in NegritoCaixaAlta.BOLD_FLAGS
+        return flags in BoldUpperCase.BOLD_FLAGS
 
     @staticmethod
     def params(params_sep_underscore):
@@ -56,7 +56,7 @@ class NegritoCaixaAlta:
         """
         func_lis: list = []
         for criteria in params_sep_underscore.split('_'):
-            func_lis.append(eval("NegritoCaixaAlta.dict_{}".format(criteria)))
+            func_lis.append(eval("BoldUpperCase.dict_{}".format(criteria)))
 
         def and_funcs(arg):
             return functools.reduce(lambda a, b: a and b,
