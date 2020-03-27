005709f0cd67b1f58a972d5e80c5eda9911f794a
Khalil Carsten <khalilcarsten@gmail.com>
2020-03-27 12:42:09 -0300

    Extract content to json

--- dodfminer/__main__.py
+++ dodfminer/__main__.py
@@ -2,7 +2,7 @@
 
 from cli import CLI
 from fetcher import Fetcher
-
+from content_extractor import ContentExtractor
 
 class Miner(object):
     """."""
@@ -13,12 +13,21 @@ class Miner(object):
         self.start_date = args.start_date
         self.end_date = args.end_date
         self.single = args.single
+        self.ext_content = args.extract_content
 
     def download(self):
         """."""
         fetcher = Fetcher(single=self.single)
         fetcher.pull(self.start_date, self.end_date)
 
+    def extract_content(self):
+        """."""
+        ContentExtractor.extract_to_json()
+
 
 if __name__ == '__main__':
-    Miner().download()
+    miner = Miner()
+    if miner.ext_content:
+        miner.extract_content()
+    else:
+        miner.download()
