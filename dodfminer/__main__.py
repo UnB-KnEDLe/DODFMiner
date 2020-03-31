9e5763712c7d4a8dbd174c577d21a0fe97c782a4
Renato Avellar Nobre <rekanobre@gmail.com>
2020-03-31 17:45:00 -0300

    Adding documentation for cli, main, and fetcher

--- dodfminer/__main__.py
+++ dodfminer/__main__.py
@@ -1,14 +1,34 @@
-"""."""
+"""Main application module.
+
+Contains class miner which is an interface to handle all extraction tasks.
+
+Typical usage example:
+    From DODFMiner execute:
+    python3 dodfminer
+
+"""
 
 from cli import CLI
 from downloader.fetcher import Fetcher
 from extract.content_extractor import ContentExtractor
 
+
 class Miner(object):
-    """."""
+    """Main DODFMiner class.
+
+    The Miner class is a interface that handles download, pre-extraction,
+    and extraction of DODFs.
+
+    Attributes:
+        start_date: Argument start_date from CLI
+        end_date: Argument end_date from CLI
+        single: Argument single from CLI
+        ext_content: Argument extract_content from CLI
+
+    """
 
     def __init__(self):
-        """."""
+        """Init Miner class to handle all the extraction process."""
         args = CLI().parse()
         self.start_date = args.start_date
         self.end_date = args.end_date
@@ -16,17 +36,16 @@ class Miner(object):
         self.ext_content = args.extract_content
 
     def download(self):
-        """."""
+        """Download PDFs with parameters from CLI."""
         fetcher = Fetcher(single=self.single)
         fetcher.pull(self.start_date, self.end_date)
 
     def extract_content(self):
-        """."""
+        """Extract Content from PDFs."""
         ContentExtractor.extract_to_json()
 
 
 if __name__ == '__main__':
-
     miner = Miner()
     if miner.ext_content:
         miner.extract_content()
--- dodfminer/downloader/fetcher.py
+++ dodfminer/downloader/fetcher.py
@@ -1,4 +1,12 @@
-"""."""
+"""Download DODFs from the Buriti Website and save on proper directory.
+
+Download monthly pdfs of DODFs, or just one in necessity of tests.
+
+Typical usage example:
+    fetcher = Fetcher(single=False)
+    fetcher.pull(start_date, end_date)
+
+"""
 
 import os
 import tqdm
@@ -17,15 +25,39 @@ MONTHS_STRING = ["", "01_Janeiro", "02_Fevereiro", "03_Março", "04_Abril",
 
 
 class Fetcher(object):
-    """Responsible for download the DODF Pdfs."""
+    """Responsible for download the DODF Pdfs.
+
+    Attributes:
+        save_path: Path to save the downloads.
+        download_path: Folder in which the downloads will be stored.
+        prog_bar: Indicate if download should contain a progress bar.
+
+    """
 
     def __init__(self, single=False):
-        """."""
+        """Construct Method for the Fetcher class.
+
+        Args:
+            single: Boolean indicating to download one or more DODFs
+
+        """
         self.save_path = './data'
         self.download_path = os.path.join(self.save_path, 'dodfs')
         self.prog_bar = None
 
     def _string_to_date(self, date):
+        """Convert the date to datetime.
+
+        Args:
+            date: The date to be converted in string format.
+
+        Returns:
+            Return the date formated in string now as datetime datatype.
+
+        Raises:
+            Exception: date passed through cli is in wrong format.
+
+        """
         if '/' in date:
             date = datetime.strptime(date, '%m/%y').date()
         elif '-' in date:
@@ -37,6 +69,18 @@ class Fetcher(object):
         return date
 
     def _create_single_folder(self, path):
+        """Create a single folder given the directory path.
+
+        This function might create a folder, observe that the folder already
+        exists, or raise an error if the folder cannot be created.
+
+        Args:
+            path: The path to be created
+
+        Raises:
+            OSError: Error creating the directory.
+
+        """
         if os.path.exists(path):
             self._log(os.path.basename(path) + " folder already exist")
         else:
@@ -50,9 +94,21 @@ class Fetcher(object):
                 self._log(basename + " directory successful created")
 
     def _create_download_folder(self):
+        """Create Downloaded DODFs Structures."""
         self._create_single_folder(self.download_path)
 
     def _make_url(self, date):
+        """Make the url to download the dodf.
+
+        Uses the date as parameter to download.
+
+        Args:
+            date: The date to download the DODF
+
+        Returns:
+            The complete url to the buriti website to download the DODF.
+
+        """
         url_string = "http://www.buriti.df.gov.br/ftp/default.asp?ano="
         url_string += str(date.year)
         url_string += "&mes=" + str(MONTHS_STRING[date.month])
@@ -62,6 +118,15 @@ class Fetcher(object):
         return url
 
     def _make_href_url(self, href):
+        """Preprocess the URL to be aceptable by the souplink.
+
+        Args:
+            href: The dodf url part.
+
+        Returns:
+            The desired url preprocessed.
+
+        """
         url = "http://www.buriti.df.gov.br/ftp/"
         url += href
         url = urllib.parse.quote(url, safe=':/?=&')
@@ -72,6 +137,15 @@ class Fetcher(object):
         return url
 
     def _make_download_url(self, href):
+        """Make downloadable url.
+
+        Args:
+            href: The dodf part of url to be downloaded
+
+        Returns:
+            The url of the dodf to download
+
+        """
         url = "http://www.buriti.df.gov.br/ftp/"
         download_url = url + href
         download_url = urllib.parse.quote(download_url, safe=':/?=&')
@@ -79,12 +153,29 @@ class Fetcher(object):
         return download_url
 
     def _fail_request_message(self, url, error):
+        """Log error messages in download.
+
+        Args:
+            url: The failing url to the website.
+            error: The kind of error happening
+
+        """
         self._log(error)
         message = "Please check your internet connection, and " \
                   "check if the url is online via browser: {}".format(url)
         self._log(message)
 
     def _get_soup_link(self, url):
+        """Create the souplink to download the pdf.
+
+        Args:
+            url: The website url to download the DODF
+        Returns:
+            An BeautifulSoup object which html queries are made
+        Raises:
+            RequestException: log error in download
+
+        """
         headers = {'User-Agent': 'Chrome/71.0.3578.80'}
         try:
             response = requests.get(url, headers=headers)
@@ -93,6 +184,17 @@ class Fetcher(object):
             self._fail_request_message(url, error)
 
     def _file_exist(self, path):
+        """Check if a file exists.
+
+        Prevents redownloads.
+
+        Args:
+            path: The path where the file might be
+
+        Returns:
+            Boolean indicating if file does really exists.
+
+        """
         if os.path.exists(path):
             self._log(os.path.basename(path) + " file already exist")
             return True
@@ -100,6 +202,19 @@ class Fetcher(object):
             return False
 
     def _download_pdf(self, url, path):
+        """Download the DODF PDF.
+
+        Note:
+            Might be time consuming depending on bandwidth.
+
+        Args:
+            url: the pdf url
+            path: the path to save the pdf
+
+        Raises:
+            RequestException: Error in case the request to download fails
+
+        """
         try:
             response = requests.get(url)
         except requests.exceptions.RequestException as error:
@@ -110,6 +225,16 @@ class Fetcher(object):
             self._log("Finished " + os.path.basename(path))
 
     def _make_month_path(self, year, actual_date):
+        """Create and return the folder for the year and month being download.
+
+        Args:
+            year: The year respective to the folder.
+            actual_date: The date in which the downloaded DODF corresponds.
+
+        Returns:
+            The path to the actual month in which the download is being made.
+
+        """
         year_path = os.path.join(self.download_path,
                                  str(actual_date.year))
         if year != actual_date.year:
@@ -130,27 +255,34 @@ class Fetcher(object):
             be changed due to some nonsense software engineer decision.
 
         Args:
-            start_date (str): the start date in format mm/yy
-            end_date (str): the start date in format mm/yy
+            start_date: the start date in format mm/yy.
+            end_date: the start date in format mm/yy.
 
         """
+        # Convert string to datetime and calculate ammount to be used in
+        # progress bar
         start_date = self._string_to_date(start_date)
         end_date = self._string_to_date(end_date)
         months_amt = ((end_date.year - start_date.year) * 12
                       + (end_date.month - start_date.month))
+        # Creates progress bar
         self.prog_bar = tqdm.tqdm(total=months_amt+1, position=0)
+        # Creates the project folder structure
         self._create_download_folder()
         year = 0
         for month in range(months_amt+1):
+            # Uses the function relative delta for in cases the date is the
+            # last of the month, increase month instead of adding more dates
             actual_date = start_date + relativedelta(months=+month)
+            # Convert back to string to update progress bar
             desc_bar = str(actual_date)
             self.prog_bar.set_description("Date %s" % desc_bar)
+            # Create and return the path for the dodfs to be donwloaded
             month_path = self._make_month_path(year, actual_date)
             self._create_single_folder(month_path)
             url = self._make_url(actual_date)
             a_list = self._get_soup_link(url)
             year = actual_date.year
-
             for a in a_list.find_all('a', href=True):
                 a_url = self._make_href_url(a['href'])
                 download_page = self._get_soup_link(a_url)
@@ -172,6 +304,7 @@ class Fetcher(object):
                                   + os.path.basename(dodf_name_path)
                                   + " jumping to the next")
 
+            # TODO: For some reason this crashes, FIXME
             self.prog_bar.update(1)
 
     def _log(self, message):
