88f086e4045631c9b84e23636b9a273af2a192ef
maffei2443 <leoitu22hotmail.com@gmail.com>
2020-06-03 22:51:08 -0300

    fix for 2018 pdf test

--- dodfminer/hierarchy.py
+++ dodfminer/hierarchy.py
@@ -36,19 +36,13 @@ class TextBlockTrans(NamedTuple):
         ret.append('\tpage, pwidth: {}'.format(self[-2:]))
         return '\n'.join(ret)
 
-TEST_PATH = Path('PDF_TITLES/')
-TEST_FILES = [TEST_PATH/Path(path) for path in os.listdir(TEST_PATH) if path.endswith('.pdf')]
-PATH = TEST_FILES[0]
 _TRASH_EXPRESSIONS = [
     "SUMÁRIO",
     "DIÁRIO OFICIAL",
     "SEÇÃO (I|II|III)",
 ]
-
 _TRASH_COMPILED = re.compile('|'.join(_TRASH_EXPRESSIONS))
 
-print('PDF file:', PATH)
-doc = fitz.open(PATH)
 
 def is_bold(flags):
     return flags & 2 ** 4
@@ -63,25 +57,6 @@ def textBlock_to_textblocktrans(lis: List[Tuple[float, float, float, float, str,
     """
     return [TextBlockTrans(*i[:-1], pnum, page_width) for i in lis]
 
-def text_blocks_transform(text_blocks: List, keep_page_width=True):
-    lis = []
-    for idx, tb in enumerate(text_blocks):
-        p_num = tb[-2]
-        p_num = tb.page
-
-        p_width = tb[-1]
-        p_width = tb.pwidth
-
-        p_num *= 2
-        x0, y0, x1, y1 = tb[:4]
-        x0 = tb.x0
-
-        p_num += int(x0 >( p_width / 2))
-        if keep_page_width:            
-            lis.append( TextBlockTrans( *(tb[:-2]), p_num, p_width ) )
-        else:
-            lis.append( TextBlockTrans( *(tb[:-2]), p_num, ) )
-    return lis
 
 def page_transform(extracted_blocks, keep_page_width=True, inplace=False):
     """Increases page numbers of extracted_blocks.
@@ -112,7 +87,38 @@ def page_transform(extracted_blocks, keep_page_width=True, inplace=False):
         d['page'] = p_num
     return extracted_blocks
 
+
+def text_blocks_transform(text_blocks: List, keep_page_width=True):
+    """Similar to `page_transform`.
+
+    Do the same as `page_transform` but `text_blocks` is expected to
+    contain `TextBlockTrans` instances insted of `dict` instances as
+    in `page_transform`. A big difference is the list returned, which
+    is NOT the same as `text_blocks`.
+    """
+    lis = []
+    for idx, tb in enumerate(text_blocks):
+        p_num = tb.page
+        p_width = tb.pwidth
+
+        p_num *= 2
+        p_num += int(tb.x0 > (p_width / 2))
+        if keep_page_width:            
+            lis.append( TextBlockTrans( *(tb[:-2]), p_num, p_width ) )
+        else:
+            lis.append( TextBlockTrans( *(tb[:-2]), p_num, ) )
+    return lis
+
+
 def is_title_subtitle(span):
+    """Evaluates to True if span['text'] is a title or subtitle.
+
+    Args:
+        span: `dict` with keys ('flags', 'text', 'font')
+    Returns:
+        True if span['text'] can be part of a title or subtitle.
+        False otherwise.
+    """
     return ((title_filter.BoldUpperCase.dict_text(span))
             and is_bold(span['flags'])
             and not re.search(_TRASH_COMPILED, span['text'])
@@ -120,9 +126,24 @@ def is_title_subtitle(span):
         )
 
 def are_title_subtitle(span_list):
+    """Convenince function to apply `is_title_subtitle` over an iterable.
+    Args:
+        span_list: an iterable of `dict` each of which having the same keys
+            expected by `is_title_subtitle`
+    Returns:
+        a `list` containing the results of applying `is_title_subtitle`
+        on each element in `span_list`.
+    """
     return [is_title_subtitle(span) for span in span_list]
 
 def get_block_spans(block):
+    """Returns all spans giver a block (a `block` as returned by fitz.TextPage.extractDICT).
+
+    Args:
+        block: text block as described at https://pymupdf.readthedocs.io/en/latest/textpage/#dictionary-structure-of-extractdict-and-extractrawdict
+    Returns:
+        all `spans` inside `block`
+    """
     span_lis = [] 
     for line in block['lines']: 
         for span in line['spans']: 
@@ -136,25 +157,25 @@ def reading_sort_dict(lis):
     return sorted(lis, key=lambda x: (x['page'], int(x['bbox'][1]), x['bbox'][0]))
 
 def drop_dup_tbt(lis: List[TextBlockTrans]):
-    """. This fun
+    """Assumes elements with same (int(x0), int(y0)) coordinates are the same and keeps only one.
+    
 
     Sometimes, a span text apears multiple times, as if there exists
     multiple spans starting at the same point. Tihs function drops
     duplicate which matches this case.
+    Args:
+        lis: a sequence of `TextBlockTrans` instances
+    Returns:
+        a `list` removing duplicated (as defined above) instances.
+
+    NOTE: please note that all `lis` elements are assumed to be on the SAME page.
+    If this is not the case, then the final result may not be what was expected.
     """
     dic = {}
     for tup in lis:
         dic[tuple([int(i) for i in tup[:2]])] = tup
     return list(dic.values())
 
-def drop_header_footer(lis: List[tuple]):
-    y0l = [ x.y0 for x in lis ]
-    mi, ma = min(y0l), max(y0l)
-    idx_mi, idx_ma = y0l.index(mi), y0l.index(ma)
-    left, right = min(idx_mi, idx_ma), max(idx_mi, idx_ma)
-    del lis[left]
-    del lis[right-1]
-    return lis
 
 def drop_header_footer_smart(lis: List[tuple], page_height=None, page_width=None):
 
@@ -189,10 +210,11 @@ def drop_header_footer_smart(lis: List[tuple], page_height=None, page_width=None
             # into account.
             del lis[ma_idx if not ma_idx else ma_idx - shrinked]
 
-    # NOTE: it is possible to do the check for header em footer
+    # NOTE: it is possible to do the check for header and footer
     # nearly using only text. However, it is computationally
     # expensive.
     return lis
+
 
 def get_first_title_cands(extracted_blocks, page_width):
     """Returns first_title candidates.
@@ -244,91 +266,6 @@ def init_hier_final(doc: fitz.Document):
     return current_section, hier, title_size
 
 
-def mount_doc_hierarchy_3(doc: fitz.Document):
-    """Improved version of `mount_doc_hierarchy2`, now taking into account
-    subtitles in hierarchy.
-
-    Returns:
-        Dict[str, List[Dict[str, List[str]]]]
-
-    """
-    current_section, hier, TITLE_SIZE = init_hier_final(doc)
-
-    prev_font_size = 0
-    prev_spans = []
-    for p_num, page in enumerate(doc):
-        p_width = page.MediaBox[2]
-
-        text_blocks = page.getTextBlocks()
-        extracted_blocks = page.getTextPage().extractDICT()['blocks']
-
-        tb_paged = textBlock_to_textblocktrans(text_blocks, p_width, p_num)    
-        tb_trans = text_blocks_transform(tb_paged, keep_page_width=False)
-
-        # cleaned_and_sorted = drop_header_footer(
-        cleaned_and_sorted = drop_header_footer_smart(
-            reading_sort_tuple(
-            drop_dup_tbt(
-                tb_trans
-            )),
-            page_height=page.MediaBox[3],
-            page_width=page.MediaBox[2]
-        )
-
-        for text_block in cleaned_and_sorted:
-
-            spans = get_block_spans(extracted_blocks[text_block.block_no])            
-            if not spans:
-                raise ValueError("Empty spans list: This message should never be shown.")
-
-            # `page_transform` espera que as chaves `page` e `page_width` existam.
-            for sp in spans:
-                sp['page'] = p_num
-                sp['page_width'] = doc[p_num].MediaBox[2]
-            
-            # `reading_sorts`espera que haja chave `page`, além de `bbox`
-            spans = reading_sort_dict(page_transform(spans))
-
-            first = spans[0]
-            first_size = first['size']         
-            first_text = first['text'] 
-            if is_bold(first['flags']) \
-                and first_text.endswith('I') and first_text.startswith('SEÇÃO I'):
-                print('SEÇÃO: \n\t"{}"'.format(first))
-                current_section = first_text
-                hier[current_section] = []
-                continue
-
-            not_fake = [ not re.match(_TRASH_COMPILED, sp['text']) for sp in spans]
-
-            section_hier = hier[current_section]
-            if all(are_title_subtitle(spans)) and first_size == TITLE_SIZE and all(not_fake):  # 
-                if first_size == prev_font_size:                    
-                    print("[TITLE] EXTENDING {} BY {}".format(section_hier[-1][0], text_block.text))
-                    section_hier[-1][0].extend([text_block.text])
-                else:   # Title doesn't extend the previous one  
-                    section_hier.append( [[text_block.text], []] )                
-            
-            else:   # another block inside a title. Check if `first` has bold font
-                if is_bold(first['flags']):
-                    for sp in spans:
-                        txt = sp['text']
-                        if txt == txt.upper(): # subtitle
-                            if prev_font_size == sp['size']:
-                                pass
-                                # section_hier[-1][]
-
-                else:
-                    section_hier[-1][1].append(text_block)
-                
-            # If there are multiples
-            prev_font_size = spans[-1]['size']
-            prev_spans = spans.copy()
-
-    aux = hier[current_section]
-    return hier
-
-
 def new_subtitle(text=''):
     return (
         [
@@ -348,9 +285,12 @@ def new_title(text=''):
     ]
 
 
-def mount_doc_hierarchy_final(doc: fitz.Document, title_debug=False, subtitle_debug=False):
-    """Improved version of `mount_doc_hierarchy2`, now taking into account
-    subtitles in hierarchy.
+def mount_doc_hierarchy_final(doc: fitz.Document,
+    title_debug=False,
+    subtitle_debug=False,
+    possile_section_debug=False,
+    section_debug=False):
+    """Mount DODF document hierarchy (secao, titulo, subtitulo and textblocks)
 
     Returns:
         Dict[str, List[Dict[str, List[str]]]]
@@ -369,21 +309,22 @@ def mount_doc_hierarchy_final(doc: fitz.Document, title_debug=False, subtitle_de
         tb_paged = textBlock_to_textblocktrans(text_blocks, p_width, p_num)    
         tb_trans = text_blocks_transform(tb_paged, keep_page_width=False)
 
-        # cleaned_and_sorted = drop_header_footer(
         cleaned_and_sorted = drop_header_footer_smart(
             reading_sort_tuple(
-            drop_dup_tbt(
-                tb_trans
-            )),
+                drop_dup_tbt(
+                    tb_trans
+                )
+            ),
             page_height=page.MediaBox[3],
             page_width=page.MediaBox[2]
         )
 
         for text_block in cleaned_and_sorted:
 
-            spans = get_block_spans(extracted_blocks[text_block.block_no])            
+            spans = get_block_spans(extracted_blocks[text_block.block_no])
+            spans = [sp for sp in spans if len(sp['text']) > 1]      
             if not spans:
-                raise ValueError("Empty spans list: This message should never be shown.")
+                continue
 
             # `page_transform` espera que as chaves `page` e `page_width` existam.
             for sp in spans:
@@ -393,40 +334,61 @@ def mount_doc_hierarchy_final(doc: fitz.Document, title_debug=False, subtitle_de
             # `reading_sorts`espera que haja chave `page`, além de `bbox`
             spans = reading_sort_dict(page_transform(spans))
 
+            
+
             first = spans[0]
             first_size = first['size']         
-            first_text = first['text'] 
+            first_text = first['text']
+            if possile_section_debug:
+                if first_text.startswith('SEÇÃO'):
+                    print("\033[92m possivel seção: ", first_text, "\033[0m")
+
+
             if is_bold(first['flags']) \
-                and first_text.endswith('I') and first_text.startswith('SEÇÃO I'):
-                # print('SEÇÃO: \n\t"{}"'.format(first))
+                and text_block.text.startswith('SEÇÃO I') and text_block.text.endswith('I'):
+                if section_debug:
+                    print('SEÇÃO: \n\t"{}"'.format(first))
                 current_section_idx = first_text
                 hier[current_section_idx] = []
                 continue
 
             not_fake = [ not re.match(_TRASH_COMPILED, sp['text']) for sp in spans]
-
             section = hier[current_section_idx]
+
             if all(are_title_subtitle(spans)) and first_size == TITLE_SIZE and all(not_fake):  # 
                 if first_size == prev_font_size:                    
-                    if title_debug:
-                        print("[TITLE] EXTENDING {} BY {}".format(section[-1][0], text_block.text))
+                    # if title_debug:
+                    print("[TITLE] EXTENDING {} BY {}".format(section[-1][0], text_block.text))
                     section[-1][0].append(text_block.text)
-                else:   # Title doesn't extend the previous one  
+                else:   # Title doesn't extend the previous one
+                    print('\033[45m\tTITLE:', text_block.text.replace('\n', '_'))
+                    print('\033[0m', end='')
                     section.append(new_title(text_block.text))                
             
             else:   # another block inside a title. Check if `first` has bold font.
                     # If is has then a subtitle is assumed.
-                if is_bold(first['flags']):
-                    for sp in spans:
-                        txt = sp['text']
-                        if txt == txt.upper() and sp['size'] < TITLE_SIZE: # subtitle
-                            if prev_font_size != first['size']:
-                                section[-1][1].append(new_subtitle())                        
-                            if subtitle_debug:
-                                print("\t[SUB-TITLE] EXTENDING {} BY {}".format(section[-1][1][-1][0], text_block.text))
-                            section[-1][1][-1][0].append(txt)
-                        else:   # remaining spans are [expected to be] ordinary ones
-                            break
+                # 
+                # if is_bold(first['flags']) and not re.match(_TRASH_COMPILED, first['text']):
+                #     # print('HUUUUUUU: ',first)
+                #     # if 'SUBSECRETARIA DA' in first['text']:
+                #     #     return first, spans
+                #     # #
+                has_sub = False
+                for sp in spans:
+                    txt = sp['text']
+                    if is_bold(sp['flags']) and not re.match(_TRASH_COMPILED, sp['text'])\
+                        and sp['size'] < TITLE_SIZE and txt == txt.upper() : # subtitle
+                        has_sub = True
+                        print('\033[79m\t[DEBUG] SUBTITLE:', txt)
+                        if prev_font_size != first['size']:
+                            section[-1][1].append(new_subtitle())                        
+                        if subtitle_debug:
+                            print("\t[SUB-TITLE] EXTENDING {} BY {}".format(section[-1][1][-1][0], text_block.text))
+                            print('\033[0m', end='')
+                        section[-1][1][-1][0].append(txt)
+                    else:   # remaining spans are [expected to be] ordinary ones
+                        break
+                if has_sub: print('                          ')
                 if not section[-1][1]:
                     section[-1][1].append(new_subtitle())
                 section[-1][1][-1][1].append(text_block.text);# section[-1][1][1].append(text_block)
@@ -463,6 +425,11 @@ def show_post_hier(hierarchy: dict):
 
 # In[38]:
 if __name__ == '__main__':
+    TEST_PATH = Path('PDF_TITLES/')
+    TEST_FILES = [TEST_PATH/Path(path) for path in os.listdir(TEST_PATH) if path.endswith('.pdf')]
+    PATH = TEST_FILES[0]
+    doc = fitz.open(PATH)
+
     sps, cands = get_first_title_cands(
         doc[0].getTextPage().extractDICT()['blocks'],
         doc[0].MediaBox[2],
@@ -481,27 +448,13 @@ if __name__ == '__main__':
     print('candidades:', cands)
     print('most-likely:', sps[cands[0][0]+1])
 
-    for filename in TEST_FILES[:1]:
+    for filename in TEST_FILES:
         # try:
         # h, dbg = mount_doc_hierarchy2(fitz.open(filename))
-        h = mount_doc_hierarchy3(fitz.open(filename))
-        # json.dump(
-        #     [i[0] for i in h], open(str(filename)[:-4]+'.json' ,'w'),
-        #     ensure_ascii=False, indent=4*' '
-        # )
+        h = mount_doc_hierarchy_final(fitz.open(filename))
+        ph = post_process_hierarchy(h)
         json.dump(
-            h, open(str(filename)[:-4]+'_hierarchy_3.json' ,'w'),
+            ph, open(str(filename)[:-4]+'_hierarchy_full.json' ,'w'),
             ensure_ascii=False, indent=4*' '
         )
-        # for idx, _ in enumerate(h):
-        #     h[idx] = (_[0], [i.text for i in _[1]])
-
-        # json.dump(
-        #     h, open(str(filename)[:-4]+'_secretarias.json' ,'w'),
-        #     ensure_ascii=False, indent=4*' '
-        # )    
         print('\t', filename, 'OK')
-        # except Exception as e:
-        #     print(e)
-        #     input()
-        #     print('FAILED', filename)
