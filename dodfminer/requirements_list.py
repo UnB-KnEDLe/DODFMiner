79e31883f8851fb7b4fc6ede85949e3e69ebef33
Khalil Carsten <khalilcarsten@gmail.com>
2020-06-09 15:44:25 -0300

    Package and Regex done

--- dodfminer/requirements_list.py
+++
@@ -1,7 +0,0 @@
-def requirements_list():
-    list_of_req = []
-    with open('requirements.txt') as req:
-        for line in req:
-            list_of_req.append(line)
-
-    return list_of_req
--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 import setuptools
-from dodfminer.requirements_list import requirements_list
+
+def requirements_list():
+    list_of_req = []
+    with open('requirements.txt') as req:
+        for line in req:
+            list_of_req.append(line)
+
+    return list_of_req
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -14,6 +21,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/UnB-KnEDLe/DODFMiner",
     packages=setuptools.find_packages(),
+    entry_points = {
+        'console_scripts': ['dodfminer=dodfminer.miner:run'],
+    },
     install_requires=requirements_list(),
     classifiers=[
         "Programming Language :: Python :: 3",
