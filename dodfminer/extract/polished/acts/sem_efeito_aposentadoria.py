eb5d918e0bca422d88f4aff3416368e01ee11b25
maffei2443 <leoitu22hotmail.com@gmail.com>
2020-07-16 16:57:17 -0300

    improve sem_efeito_aposentadoria

--- dodfminer/extract/polished/acts/sem_efeito_aposentadoria.py
+++ dodfminer/extract/polished/acts/sem_efeito_aposentadoria.py
@@ -14,8 +14,8 @@ MONTHS = (
     r'julho|agosto|setembro|outubro|novembro|dezembro)'
 )
 
-
-FLEX_DATE = r"(?P<date>\d+\s+(?:de\s*)?" + MONTHS + r"\s*(?:de\s*)?\d+|\d+[.]\d+[.]\d+|\d+[/]\d+[/]\d+)"
+FLEX_DATE = r"(?P<date>\d+\s+(?:de\s*)?" + MONTHS + \
+    r"\s*(?:de\s*)?\d+|\d+[.]\d+[.]\d+|\d+[/]\d+[/]\d+)"
 
 DODF_NUM = r"(?i:DODF|[Dd]i.rio [Oo]ficial [Dd]o [Dd]istrito [Ff]ederal)\s*[\w\W]{0,3}?(?i:n?(.mero|[.roº]{1,4})?[^\d]+?)(?P<num>\d+)"
 
@@ -64,8 +64,9 @@ class SemEfeitoAposentadoria(Atos):
         no_split_word = s.replace('-\n', '-')
         return no_split_word.replace('\n', ' ')
 
-
-    def __init__(self, file, backend, debug=False, extra_search=True, nlp=None):
+    def __init__(self, file, backend, debug=False, extra_search=True,
+                 nlp=None, max_length=2000):
+        self._max_length = max_length
         self._debug = debug
         self._extra_search = extra_search
         self._processed_text = self._pre_process_text(open(file).read())
@@ -81,13 +82,13 @@ class SemEfeitoAposentadoria(Atos):
 
     def _rule_for_inst(self):
         return (
-            r"TORNAR SEM EFEITO" +
-            r"([^\n]+\n){0,10}?[^\n]*?(tempo\sde\sservi.o|aposentadoria|" +
-            r"aposentou|([Dd][Ee][Ss])?[Aa][Vv][Ee][Rr][Bb][Aa]..[Oo]|(des)" +
-            r"?averb(ar?|ou))[\d\D]{0,500}?[.]\s(?=[A-Z]{4})"
+            r"TORNAR SEM EFEITO"
+            r"([^\n]+\n){0,10}?[^\n]*?"
+            r"(tempo\sde\sservi.o|aposentadoria|aposentou|([Dd][Ee][Ss])?[Aa][Vv][Ee][Rr][Bb][Aa]..[Oo]|(des)?averb(ar?|ou))"
+            r"[\d\D]{0,500}?[.]\s+"
+            r"(?=[A-Z]{3})"
         )
 
-    # TODO: UPDATE REULES!!!
     def _prop_rules(self):
         return {
             'tipo_documento': TIPO_DOCUMENTO,
@@ -102,19 +103,32 @@ class SemEfeitoAposentadoria(Atos):
             a list with all re.Match objects resulted from searching for
         """
         head = "TORNAR SEM EFEITO"
-        end = "CAFEBABE"
-
+        end = " CAFEBABE"   # so that lookeahead does not become a problem
         lis = self._processed_text.split(head)
         lis = [
             re.search(self._inst_rule, head + tex + end)
-            for tex in lis[1:]]
+            # content before first `head` occurrence does not matter
+            for tex in lis[1:]
+        ]
         lis = [i for i in lis if i]
-        self._raw_matches = lis
-        if self._debug:
-            print("DEBUG:", len(lis), 'matches')
-        return [i.group() for i in lis]
 
-    # TODO: UPDATE WITH SEM EFEITO APOSENTADORIA SPECIFICITIES
+        true_positive = []
+        for raw_match in lis:
+            flag = True
+            for bad in self._BAD_MATCH_WORDS:
+                s = raw_match.group()
+                if len(s) > self._max_length or bad in s:
+                    flag = False
+                    break
+            if flag:
+                true_positive.append(raw_match)
+
+        self._raw_matches = true_positive
+        if self._debug:
+            print("DEBUG:", len(lis), 'generic matches')
+            print("DEBUG:", len(self._raw_matches), 'true matches')
+        return [i.group() for i in self._raw_matches]
+
     def _get_special_acts(self, lis_dict):
         for i, match in enumerate(self._raw_matches):
             act = match.group()
@@ -122,8 +136,10 @@ class SemEfeitoAposentadoria(Atos):
             dodf_date = curr_dict['dodf_data']
             dodf_num = dodf_date and re.search(DODF_NUM, dodf_date.group())
             tornado_sem_efeito_publicacao = dodf_date and \
-                re.search(FLEX_DATE, act[:dodf_date.start()] + act[dodf_date.end():])
-            dodf_pagina = dodf_date and re.search(PAGE, act[dodf_date.end():][:50])
+                re.search(
+                    FLEX_DATE, act[:dodf_date.start()] + act[dodf_date.end():])
+            dodf_pagina = dodf_date and re.search(
+                PAGE, act[dodf_date.end():][:50])
 
             servidor = re.search(SERVIDOR_NOME_COMPLETO, act)
             if not servidor:
@@ -138,38 +154,47 @@ class SemEfeitoAposentadoria(Atos):
                     cand_text = 'SEM-SERVIDOR'
                     for cand in self._nlp(', '.join([c.strip().title() for c in all_cands])).ents:
                         cand_text = cand.text
+
                         if cand.label_ == 'PER':
                             break
                     servidor = re.search(cand_text.upper(), act)
                     del all_cands, cand_text, cand
             end_employee = servidor.end() if servidor else 0
             matricula = re.search(MATRICULA, act[end_employee:]) or \
-                        re.search(MATRICULA_GENERICO, act[end_employee:]) or \
-                        re.search(MATRICULA_ENTRE_VIRGULAS, act[end_employee:])
+                re.search(MATRICULA_GENERICO, act[end_employee:]) or \
+                re.search(MATRICULA_ENTRE_VIRGULAS, act[end_employee:])
+
             del end_employee
             if not matricula or not servidor:
                 cargo = None
             else:
-                servidor_start = act[servidor.start():].find(servidor.group()) + servidor.start()
-                matricula_start = act[matricula.start():].find(matricula.group()) + matricula.start()
+                servidor_start = act[servidor.start():].find(
+                    servidor.group()) + servidor.start()
+                matricula_start = act[matricula.start():].find(
+                    matricula.group()) + matricula.start()
 
                 # NOTE: -1 is important in case `matricula` end with `,`
                 if 0 <= (matricula_start - (servidor_start + len(servidor.group()))) <= 5:
-                    # cargo does not fit between 'servidor' e 'matricula'
-                    cargo = re.search(r",(?P<cargo>[^,]+)", act[matricula_start + len(matricula.group())-1:])
+                    # cargo does not fit between 'servidor' and 'matricula'
+                    cargo = re.search(
+                        r",(?P<cargo>[^,]+)", act[matricula_start + len(matricula.group())-1:])
+
                 else:
                     # cargo right after employee's name
-                    cargo = re.search(r",(?P<cargo>[^,]+)", act[servidor_start + len(servidor.group())-1:])
+
+                    cargo = re.search(
+                        r",(?P<cargo>[^,]+)", act[servidor_start + len(servidor.group())-1:])
                 del matricula_start, servidor_start
             edicao = re.search(DODF_TIPO_EDICAO, act)
             dodf_tipo_edicao = re.search(TIPO_EDICAO, act[edicao.start()-1:edicao.end()+1])\
-                         if edicao else re.search("normal", "normal")
+                if edicao else re.search("normal", "normal")
             curr_dict['dodf_num'] = dodf_num
             curr_dict['tornado_sem_efeito_publicacao'] = tornado_sem_efeito_publicacao
             curr_dict['dodf_pagina'] = dodf_pagina
             curr_dict['servidor'] = servidor
             curr_dict['matricula'] = matricula
             curr_dict['cargo'] = cargo
+
             curr_dict['dodf_tipo_edicao'] = dodf_tipo_edicao
 
     def _find_props(self, rule, act):
@@ -193,7 +218,7 @@ class SemEfeitoAposentadoria(Atos):
             the whole match if there are no groups at all or raise
             an exception if there are more than two groups.
         """
-        if not match or type(match) == str:
+        if not match or isinstance(match, str):
             return "nan"
         elif match.groupdict():
             key = list(match.groupdict())[0]
@@ -219,21 +244,6 @@ class SemEfeitoAposentadoria(Atos):
         _ = re.search(self._name, self._name)
         for dic in self._acts:
             dic["tipo_ato"] = _
-        data = [{k: self._group_solver(v) for k, v in act.items()} for act in self._acts]
+        data = [{k: self._group_solver(v) for k, v in act.items()}
+                for act in self._acts]
         return pd.DataFrame(data)
-
-    def _post_process_raw(self):
-        lis = []
-        for raw in self._raw_matches:
-            s = raw.group()
-            # Make sure words splitted accross lines are joined together
-            no_split_word = s.replace('-\n', '-')
-
-            # Makes easier to deal with the text.
-            single_spaces = re.sub(r'\s+', r' ', no_split_word)
-
-            # Sometimes more than one "TORNAR SEM EFEITO" is captured.
-            # Only the last one hould matter.
-            last_tornar_sem_efeito = single_spaces[single_spaces.rfind("TORNAR SEM EFEITO"):]
-            lis.append(last_tornar_sem_efeito)
-        return lis
