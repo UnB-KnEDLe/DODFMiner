92c8f12a9f9f9ea604ad5550c55f5e592355e424
notopoloko <amarals16@hotmail.com>
2022-01-09 21:50:37 -0300

    Fix numpy version

--- dodfminer/__version__.py
+++ dodfminer/__version__.py
@@ -5,7 +5,7 @@ version_info = (1, 3, 12)
 
 def get_version():
     "Returns the version as a human-format string."
-    return f'{version_info[0]}.{version_info[1]}.{version_info[2]}rc2'
+    return f'{version_info[0]}.{version_info[1]}.{version_info[2]}rc5'
 
 
 __version__ = get_version()
