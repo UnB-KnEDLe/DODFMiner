ad6176eac49eda6c881f349ad401e5aa424486e9
brunoedcf <brest.dallacosta@outlook.com>
2022-10-17 18:37:54 -0300

    Replicar a função de extração para os atos Licitação, Suspensão, Aditamento, Anulação

--- dodfminer/extract/polished/acts/suspensao.py
+++ dodfminer/extract/polished/acts/suspensao.py
@@ -6,41 +6,40 @@ import nltk
 from nltk.tokenize import word_tokenize
 nltk.download('punkt')
 
-from dodfminer.extract.polished.acts.base import Atos
-
-class Suspensao():
+class Licitacao():
 
     def __init__(self, file, backend):
-        self.filename = file
-        self.file = file
         self.backend = backend
         self.model = None
-        self.texts = []
-        self.preds = []
-        self.df = []
-        self.flow()
 
+        self.filename = file
+        self.file = None
+        
+        self.atos_encontrados = []
+        self.predicoes = []
+
+        self.df = []
+
+        # Inicializar fluxo
+        self.flow()
 
     def flow(self):
         self.load()
-        self.pre_process()
+        # self.pre_process()
         self.ner_extraction()
         self.post_process()
-        return self.df
-    
     
     def load(self):
-        self.model = joblib.load('./models/suspensao.pkl')
-        if self.file[-5:] == '.json':
-            with open(self.file, 'r') as f:
+        self.model = joblib.load('CAMINHO PARA O MODELO')
+        if self.filename[-5:] == '.json':
+            with open(self.filename, 'r') as f:
                 self.file = json.load(f)
-            self.file = self.segment(self.file)
+            self.atos_encontrados = self.segment(self.file)
         else:
             pass
 
-
     def segment(self, file):
-        atos_suspensao = []
+        atos = []
 
         try:
             section_3 = file['json']['INFO']['Seção III']
@@ -49,61 +48,60 @@ class Suspensao():
                 for documento in section_3[orgao]:
                     for ato in section_3[orgao][documento]:
                         if "suspensão" in section_3[orgao][documento][ato]['titulo'].lower():
-                            atos_suspensao.append(re.sub(r'<[^>]*>', '', section_3[orgao][documento][ato]['texto']))
+                            atos.append(re.sub(r'<[^>]*>', '', section_3[orgao][documento][ato]['texto']))
 
         except KeyError:
             print("Chave 'Seção III' não encontrada")
             print(f"Chaves existentes: {file['json']['INFO'].keys()}")
 
-        return atos_suspensao
+        return atos
 
+    # def pre_process(self):
+    #     self.texts = self.atos_encontrados
+    #     # Tokenize
+    #     self.texts = [word_tokenize(x) for x in self.texts]
+    #     # Get Features
+    #     self.texts = [self.get_features(x) for x in self.texts]
 
-    def pre_process(self):
-        self.texts = self.file
-        self.file = [word_tokenize(x) for x in self.file]
-        self.file = [self.get_features(x) for x in self.file]
+    # def get_features(self, sentence):
         
+    #     sent_features = []
+    #     for i in range(len(sentence)):
+    #         # print(sentence[i])
+    #         word_feat = {
+    #             # Palavra atual
+    #             'word': sentence[i].lower(),
+    #             'capital_letter': sentence[i][0].isupper(),
+    #             'all_capital': sentence[i].isupper(),
+    #             'isdigit': sentence[i].isdigit(),
+    #             # Uma palavra antes
+    #             'word_before': '' if i == 0 else sentence[i-1].lower(),
+    #             'word_before_isdigit': '' if i == 0 else sentence[i-1].isdigit(),
+    #             'word_before_isupper': '' if i == 0 else sentence[i-1].isupper(),
+    #             'word_before_istitle': '' if i == 0 else sentence[i-1].istitle(),
+    #             # Uma palavra depois
+    #             'word_after': '' if i+1 >= len(sentence) else sentence[i+1].lower(),
+    #             'word_after_isdigit': '' if i+1 >= len(sentence) else sentence[i+1].isdigit(),
+    #             'word_after_isupper': '' if i+1 >= len(sentence) else sentence[i+1].isupper(),
+    #             'word_after_istitle': '' if i+1 >= len(sentence) else sentence[i+1].istitle(),
 
-    def get_features(self, sentence):
-        
-        sent_features = []
-        for i in range(len(sentence)):
-            # print(sentence[i])
-            word_feat = {
-                # Palavra atual
-                'word': sentence[i].lower(),
-                'capital_letter': sentence[i][0].isupper(),
-                'all_capital': sentence[i].isupper(),
-                'isdigit': sentence[i].isdigit(),
-                # Uma palavra antes
-                'word_before': '' if i == 0 else sentence[i-1].lower(),
-                'word_before_isdigit': '' if i == 0 else sentence[i-1].isdigit(),
-                'word_before_isupper': '' if i == 0 else sentence[i-1].isupper(),
-                'word_before_istitle': '' if i == 0 else sentence[i-1].istitle(),
-                # Uma palavra depois
-                'word_after': '' if i+1 >= len(sentence) else sentence[i+1].lower(),
-                'word_after_isdigit': '' if i+1 >= len(sentence) else sentence[i+1].isdigit(),
-                'word_after_isupper': '' if i+1 >= len(sentence) else sentence[i+1].isupper(),
-                'word_after_istitle': '' if i+1 >= len(sentence) else sentence[i+1].istitle(),
+    #             'BOS': i == 0,
+    #             'EOS': i == len(sentence)-1
+    #         }
+    #         sent_features.append(word_feat)
 
-                'BOS': i == 0,
-                'EOS': i == len(sentence)-1
-            }
-            sent_features.append(word_feat)
+    #     return sent_features
 
-        return sent_features
-
-
+    #corrigir palavra extraction
     def ner_extraction(self):
-        for t in self.file:
+        for t in self.atos_encontrados:
           pred = self.model.predict([t])
-          pred = pred[0]
-          self.preds.append(pred)
+          self.predicoes.append(pred)
 
-
+    # Montar dataframe com as predições e seus IOB's
     def post_process(self):
 
-        for IOB, text in zip(self.preds, self.texts):
+        for IOB, text in zip(self.predicoes, self.atos_encontrados):
 
             ent_dict = {
               'ato': '',
@@ -148,4 +146,4 @@ class Suspensao():
             self.df.append(ent_dict)
 
         self.df = pd.DataFrame(self.df)
-        self.df.to_csv('result.csv',na_rep='NaN')
+        # self.df.to_csv('result.csv',na_rep='NaN')
